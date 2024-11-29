7ef5fa7e3db9d5f244a13b29c6c1af41e62a8e25
Anir-sadiqui <anirsadiqui2@gmail.com>
2024-11-29 01:31:53 +0100

    modif

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:9090',
+    baseURL: 'http://172.31.250.60:9090',
     timeout: 1000,
     headers: {'Authorization': 'Bearer yourToken'}
 });
