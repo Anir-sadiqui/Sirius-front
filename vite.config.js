39f8004d9b0013a6f8a1db6fb5c642c2732fc50d
Anir-sadiqui <anirsadiqui2@gmail.com>
2024-11-29 15:29:07 +0100

    Jfile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://172.31.250.60:9090',
+        changeOrigin: true,
+      },
+    },
+  },
+});
