696e19535f8e9d180c786bf2328ea9d4e27abd36
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-11-29 21:30:51 +0100

    add .env for the env variables for the API_BASE_URL for the local and the frontend VM

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
+// Dynamically set the baseURL based on environment
+const baseURL =
+    import.meta.env.MODE === 'development'
+        ? import.meta.env.VITE_API_BASE_URL_LOCAL
+        : import.meta.env.VITE_API_BASE_URL_VM;
+
+// Configure Axios instance
 const api = axios.create({
-    baseURL: 'http://172.31.250.60:9090',
+    baseURL: baseURL,
     timeout: 1000,
-    headers: {'Authorization': 'Bearer yourToken'}
+    headers: { 'Authorization': 'Bearer yourToken' },
 });
 
 export default api;
